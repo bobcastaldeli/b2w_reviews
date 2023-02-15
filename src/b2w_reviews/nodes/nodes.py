62a0b7236319617099cb657309b029b6bd84dc75
bobcastaldeli <castaldelliroberto@gmail.com>
2023-02-14 23:47:17 -0300

    feat: add functions in data engineering nodes

--- src/b2w_reviews/nodes/nodes.py
+++ src/b2w_reviews/nodes/nodes.py
@@ -3,19 +3,51 @@ This script contains nodes containing functions for pipeline
 """
 
 import datasets
+import pandas as pd
 
 
-def load_reviews_dataset(dataset: str):
+def load_reviews_dataset(dataset: str, path: str):
     """
     Load the reviews dataset from the datasets library. 
     and save in a csv file in data folder
     
     Args:
         dataset: dataset to be loaded
+        path: path to the reviews dataset
     
     Returns:
         reviews: B2W reviews dataset from huggingface datasets library
     """
     # load the dataset
     reviews = datasets.load_dataset(dataset)
-    return reviews['train'].to_csv('../data/01_raw/reviews.csv', index=False)
+    return reviews['train'].to_csv(path, index=False)
+
+
+def read_reviews_dataset(path: str):
+    """
+    Read the reviews dataset from the data folder
+    
+    Args:
+        path: path to the reviews dataset
+    
+    Returns:
+        reviews: B2W reviews dataset
+    """
+    # read the dataset
+    reviews = pd.read_csv(path)
+    return reviews
+
+
+def drop_null_values(dataframe: pd.DataFrame, column: str):
+    """
+    Drop all the rows with null values in a column
+    
+    Args:
+        dataframe: dataframe to be cleaned
+        column: column to be checked
+    
+    Returns:
+        dataframe: cleaned dataframe
+    """
+    dataframe = dataframe.dropna(subset=[column])
+    return dataframe
